8f7de9aa45184525902b8c43a9c254c103eac9c4
bmanek <41875460+bmanek@users.noreply.github.com>
2019-06-28 16:27:43 -0400

    add create route and corresponding controller method for session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,5 +4,15 @@ class Api::V1::SessionsController < ApplicationController
     @sessions = Session.all
     render json: @sessions
   end
-  
+
+  def update
+    byebug
+  end
+
+  def create
+    @session = Session.create(user_id: params["user_id"], game_type:
+      params["game_type"], total_questions: params["total_questions"],
+      number_wrong: params["number_wrong"], type_wrong: params["type_wrong"])
+  end
+
 end
