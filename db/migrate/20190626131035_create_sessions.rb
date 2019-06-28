2cd99e656b9bf75d02a39eec09adb291d5890048
bmanek <41875460+bmanek@users.noreply.github.com>
2019-06-28 15:24:57 -0400

    change session migrations to include total_questions, number_wrong, and type_wrong

--- db/migrate/20190626131035_create_sessions.rb
+++ db/migrate/20190626131035_create_sessions.rb
@@ -2,9 +2,20 @@ class CreateSessions < ActiveRecord::Migration[6.0]
   def change
     create_table :sessions do |t|
       t.string :game_type
-      t.integer :score
+      t.integer :total_questions
+      t.integer :number_wrong
+      t.string :type_wrong
 
       t.timestamps
     end
   end
 end
+
+
+# what data does one session keep track of?
+# - total no. of questions
+# - total no. incorrect
+# - key OR interval of incorrect question
+#
+# CURRENT plan is to make Keys and Intervals separate tables and connect
+# each to sessions thru many to many joins.
