c5d7f896a4026102f7f7ad0a9fab1479e7b623d4
bmanek <41875460+bmanek@users.noreply.github.com>
2019-07-01 12:33:40 -0400

    resolve Unexpected end of JSON input error - needed a response object in sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -13,6 +13,8 @@ class Api::V1::SessionsController < ApplicationController
     @session = Session.create(user_id: params["user_id"], game_type:
       params["game_type"], total_questions: params["total_questions"],
       number_wrong: params["number_wrong"], type_wrong: params["type_wrong"])
+
+      render json: {message: "Try harder..."}
   end
 
 end
